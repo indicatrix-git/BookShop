e97f618417cc0cae448dc1b9606a14421108528d
Martin Timothy Timko <complex.indicatrix@gmail.com>
2026-02-15 21:25:40 +0100

    Add files via upload

---
+++ BookShop/ViewModels/CandyListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.ViewModels
+{
+    public class CandyListViewModel
+    {
+        public IEnumerable<Candy> Candies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
---
+++ BookShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Candy> CandyOnSale { get; set; }
+    }
+}
